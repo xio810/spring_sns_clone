032d2e9d5bc0936f02ef8b005a0fdbc2eda362fd
xio810 <boi4806@naver.com>
2022-01-08 16:02:31 +0900

    세팅

---
+++ src/main/java/com/xio/clone/SnsCloneApplication.java
@@ -0,0 +1,13 @@
+package com.xio.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xio/clone/SnsCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xio.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

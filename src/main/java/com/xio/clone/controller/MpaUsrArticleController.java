9cd7a04071e6ad4151fd8588a726c99a44b5bbad
xio810 <boi4806@naver.com>
2022-01-10 14:56:09 +0900

    ResultData 도입

--- src/main/java/com/xio/clone/controller/MpaUsrArticleController.java
+++ src/main/java/com/xio/clone/controller/MpaUsrArticleController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.xio.clone.util.Util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 @Controller
 public class MpaUsrArticleController {
 
@@ -13,7 +16,7 @@ public class MpaUsrArticleController {
 
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
-	public Article doWrite(String title, String body) {
+	public ResultData doWrite(String title, String body) {
 		int id = articleLastId + 1;
 		String regDate = Util.getNowDateStr();
 		String updateDate = Util.getNowDateStr();
@@ -21,65 +24,28 @@ public class MpaUsrArticleController {
 		Article article = new Article(id, regDate, updateDate, title, body);
 
 		articleLastId = id;
-
-		return article;
+		
+		ResultData resultData = new ResultData("s-1", id + "번 게시물이 등록되었습니다.", article);
+		
+		return resultData;
 	}
 }
 
+@AllArgsConstructor
+@Data
+class ResultData {
+	private String resultCode;
+	private String msg;
+	private Article article;
+}
+
+@AllArgsConstructor
+@Data
 class Article {
 	private int id;
 	private String regDate;
 	private String updateDate;
 	private String title;
 	private String body;
-	
-	public Article(int id, String regDate, String updateDate, String title, String body) {
-		super();
-		this.id = id;
-		this.regDate = regDate;
-		this.updateDate = updateDate;
-		this.title = title;
-		this.body = body;
-	}
-	
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getRegDate() {
-		return regDate;
-	}
-
-	public void setRegDate(String regDate) {
-		this.regDate = regDate;
-	}
-
-	public String getUpdateDate() {
-		return updateDate;
-	}
-
-	public void setUpdateDate(String updateDate) {
-		this.updateDate = updateDate;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getBody() {
-		return body;
-	}
-
-	public void setBody(String body) {
-		this.body = body;
-	}
 }

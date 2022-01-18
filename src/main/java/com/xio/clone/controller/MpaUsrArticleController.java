3426cb0f6c03aebf2c4c337fa76b446a4258f6d3
xio810 <boi4806@naver.com>
2022-01-18 18:36:16 +0900

    컨트롤러에서 입력데이터 유효성 체크

--- src/main/java/com/xio/clone/controller/MpaUsrArticleController.java
+++ src/main/java/com/xio/clone/controller/MpaUsrArticleController.java
@@ -15,7 +15,6 @@ import ch.qos.logback.core.joran.util.ConfigurationWatchListUtil;
 
 @Controller
 public class MpaUsrArticleController {
-
 	private List<Article> articles;
 	private int articleLastId;
 
@@ -28,73 +27,107 @@ public class MpaUsrArticleController {
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
+
+		if (Util.isEmpty(title)) {
+			return new ResultData("F-1", "제목을 입력해주세요.");
+		}
+
+		if (Util.isEmpty(body)) {
+			return new ResultData("F-2", "내용을 입력해주세요.");
+		}
+
 		int id = writeArticle(title, body);
 		Article article = getArticleById(id);
 
 		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
 
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public ResultData doModify(Integer id, String title, String body) {
+
+		if (Util.isEmpty(id)) {
+			return new ResultData("F-1", "번호를 입력해주세요.");
+		}
+
+		if (Util.isEmpty(title)) {
+			return new ResultData("F-2", "제목을 입력해주세요.");
+		}
+
+		if (Util.isEmpty(body)) {
+			return new ResultData("F-3", "내용을 입력해주세요.");
+		}
+
+		boolean modified = modifyArticle(id, title, body);
+
+		if (modified == false) {
+			return new ResultData("F-1", id + "번 글이 존재하지 않습니다.", "id", id);
+		}
+
+		return new ResultData("S-1", id + "번 글이 수정되었습니다.", "article", getArticleById(id));
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public ResultData doDelete(Integer id) {
+		if (Util.isEmpty(id)) {
+			return new ResultData("F-1", "번호를 입력해주세요.");
+		}
+
+		boolean deleted = deleteArticleById(id);
+
+		if (deleted == false) {
+			return new ResultData("F-1", id + "번 글이 존재하지 않습니다.", "id", id);
+		}
+
+		return new ResultData("S-1", id + "번 글이 삭제되었습니다.", "id", id);
+	}
+
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public ResultData getArticle(int id) {
+	public ResultData getArticle(Integer id) {
+		if (Util.isEmpty(id)) {
+			return new ResultData("F-1", "번호를 입력해주세요.");
+		}
+
 		Article article = getArticleById(id);
 
 		if (article == null) {
-			return new ResultData("F-1", id + "번 글이 없습니다.", "id", id);
+			return new ResultData("F-1", id + "번 글은 존재하지 않습니다.", "id", id);
 		}
 
 		return new ResultData("S-1", article.getId() + "번 글 입니다.", "article", article);
 	}
 
-	@RequestMapping("/usr/article/doDelete")
-	@ResponseBody
-	public ResultData doDelete(int id) {
-		boolean deleted = deleteArticleById(id);
-		
-		if (deleted == false) {
-			return new ResultData("F-1", id + "번 글이 없습니다.", "id", id);
+	// 내부
+	private void makeTestData() {
+		for (int i = 0; i < 3; i++) {
+			writeArticle("제목1", "내용1");
 		}
-		
-		return new ResultData("S-1", id + "번 글이 삭제되었습니다.",  "id", id);
 	}
-	
-	@RequestMapping("/usr/article/doModify")
-	@ResponseBody
-	public ResultData doModify(int id, String title, String body) {
-		boolean modified = modifyArticle(id,title,body);
-		
-		if (modified == false) {
-			return new ResultData("F-1", id + "번 글이 없습니다.", "id", id);
-		}
-		
-		return new ResultData(title,  id + "번 글이 수정되었습니다.", "article", getArticleById(id));
-	}
-	
+
 	private boolean modifyArticle(int id, String title, String body) {
-		
 		Article article = getArticleById(id);
-		
-		if(article == null) {
+
+		if (article == null) {
 			return false;
 		}
-		
+
 		article.setUpdateDate(Util.getNowDateStr());
 		article.setTitle(title);
 		article.setBody(body);
-		
+
 		return true;
 	}
 
 	private boolean deleteArticleById(int id) {
-
 		Article article = getArticleById(id);
-		
-		if(article == null) {
+
+		if (article == null) {
 			return false;
 		}
-		
+
 		articles.remove(article);
-		
 		return true;
 	}
 
@@ -112,7 +145,6 @@ public class MpaUsrArticleController {
 	}
 
 	private Article getArticleById(int id) {
-
 		for (Article article : articles) {
 			if (article.getId() == id) {
 				return article;
@@ -120,11 +152,5 @@ public class MpaUsrArticleController {
 		}
 
 		return null;
-	}
-
-	private void makeTestData() {
-		for (int i = 1; i < 4; i++) {
-			writeArticle("title" + i, "body" + i);
-		}
 	}
 }

33117b83c664706738a572b0b060657d5faa320c
xio810 <boi4806@naver.com>
2022-03-26 17:58:52 +0900

    멤버 컨트롤러 서비스

--- src/main/java/com/xio/clone/controller/MpaUsrMemberController.java
+++ src/main/java/com/xio/clone/controller/MpaUsrMemberController.java
@@ -1,18 +1,19 @@
 package com.xio.clone.controller;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.xio.clone.dto.Member;
+import com.xio.clone.dto.ResultData;
 import com.xio.clone.service.MemberService;
 import com.xio.clone.util.Util;
 
 @Controller
 public class MpaUsrMemberController {
-	
+
 	@Autowired
 	private MemberService memberService;
 
@@ -22,10 +23,21 @@ public class MpaUsrMemberController {
 	}
 
 	@RequestMapping("/mpaUsr/member/doJoin")
-	@ResponseBody
-	public Map doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		return Util.mapOf("loginId", loginId, "loginPw", loginPw, "name", name, "nickname", nickname, "cellphoneNo",
-				cellphoneNo, "email", email);
+	public String doJoin(HttpServletRequest req, String loginId, String loginPw, String name, String nickname,
+			String cellphoneNo, String email) {
+		Member oldMember = memberService.getMemberByLoginId(loginId);
+
+		if (oldMember != null) {
+			return Util.msgAndBack(req, loginId + "(은)는 이미 사용중인 로그인아이디 입니다.");
+		}
+
+		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+		if (joinRd.isFail()) {
+			return Util.msgAndBack(req, joinRd.getMsg());
+		}
+
+		return Util.msgAndReplace(req, joinRd.getMsg(), "/");
 	}
 
 }
